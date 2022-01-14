e68ae207f8c45bb6cb4431579b37396e68841aa7
David <davedwardz@gmail.com>
2022-01-14 09:31:55 -0800

    https://stackoverflow.com/q/70588189/1248974

---
+++ main.c
@@ -0,0 +1,31 @@
+#include "twOSPort.h"
+#include "twLogger.h"
+#include "twApi.h"
+// #include "log.h"
+#include <stdio.h>
+#include <stdio_ext.h>
+#include <string.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/time.h>
+#include <time.h>
+#include <libconfig.h>
+#include <math.h>
+#include <pthread.h>
+#include <errno.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <curl/curl.h>
+#include <stdlib.h>
+#include <portaudio.h>
+#include <sndfile.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("%s\n", "hello");
+    /* code */
+    return 0;
+}
